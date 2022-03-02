68df623dac9c59668e083e1d788221e8954ea939
João Souza <joao.souza@ice.ufjf.br>
2022-03-02 09:58:27 -0300

    fix: added cors headers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { router } from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 export { app };
